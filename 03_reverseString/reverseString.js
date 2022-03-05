4b5870675b66b9b783f764783ff0d4fd37406e73
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-04 22:37:48 -0500

    Add function logic to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(input) {
+  let stringArray = [];
+  let outputString = '';
 
+  for (let i = 0; i < input.length; i++) {
+    stringArray.push(input.slice(i, (i + 1)));
+  }
+
+  stringLength = stringArray.length;
+
+  while (stringArray.length > 0) {
+    outputString += stringArray.pop();
+  }
+
+  return outputString;
 };
 
 // Do not edit below this line
