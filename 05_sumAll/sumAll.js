dccc99c6265a9cd8aa323ebed5bc12012b56e6a3
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-05 00:25:14 -0500

    Add function logic to sum between parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(...num) {
+  let sum = 0;
 
+  num.sort(function(a, b){return a - b});
+
+  for (const arg of num) {
+    if (arg < 0 || typeof(arg) != 'number') {
+      return 'ERROR';
+    }
+  }
+
+  for (let i = num[0]; i <= num[1]; i++) {
+    sum += i;
+  }
+  
+  return sum;
 };
 
 // Do not edit below this line
