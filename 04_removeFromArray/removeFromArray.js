75c96afbdc8ee94c6d5793b8d9d876e80f768d2d
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-05 00:08:07 -0500

    Add function logic to remove array elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...num) {
+  let outputArray = [...inputArray];
 
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < num.length; j++) {
+      if (outputArray[i] === num[j]) {
+        outputArray.splice(i, 1);
+        i -= 1;
+        break;
+      }
+    }
+  }
+
+  return outputArray;
 };
 
 // Do not edit below this line
