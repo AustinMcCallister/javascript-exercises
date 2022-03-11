05668c17286b0cd3d4c86fc9ce69060a62c43c6c
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-11 11:14:03 -0500

    Add oldest person function logic

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const oldest = people.sort((a, b) => {
+    if (!a.yearOfDeath) {
+      a.yearOfDeath = (new Date()).getFullYear();
+    }
+    else if (!b.yearOfDeath) {
+      b.yearOfDeath = (new Date()).getFullYear();
+    }
+    const first = a.yearOfDeath - a.yearOfBirth;
+    const second = b.yearOfDeath - b.yearOfBirth;
+    return (first < second) ? 1 : -1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
