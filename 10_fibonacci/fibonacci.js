bbd87101241b0caa2ee1c1090c7fa6961dddb201
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-11 00:43:59 -0500

    Add fibonacci function logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let firstTerm = 0;
+  let secondTerm = 1;
+  let nextTerm;
 
+  if (n < 0) {
+    return 'OOPS';
+  }
+  else {
+    for (let i = 0; i < n; i++) {
+      nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+    return firstTerm;
+  }
 };
 
 // Do not edit below this line
