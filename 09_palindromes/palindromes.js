9ac7144f0f534b1ac4bf92df2bd1007816ec9795
Austin McCallister <austin.mccallister@protonmail.com>
2022-03-11 00:09:26 -0500

    Add palindrome function logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  forwardInput = input.replaceAll(/[!,.\s]/g, '')
+  reversedInput = input.split("").reverse().join("").replaceAll(/[!,.\s]/g, '');
+  
+  if (forwardInput.toLowerCase() === reversedInput.toLowerCase()) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 // Do not edit below this line
